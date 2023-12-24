d24466e1b11b8940ab46c99bac33d65911d37b50
Prince Soni <132805751+NyXdeity@users.noreply.github.com>
2023-12-24 22:58:37 +0530

    complex_primives added

--- index.js
+++ index.js
@@ -284,3 +284,15 @@ console.log("s % b =", s%b);//modulus
 // console.log(ans);
 
 */
+//*complex primitives://arrays and objects:-
+// WAP to print all the even numbers in an array
+
+const personArray = ["this","is","a","ARRAY"];
+console.log(personArray[0]);
+
+const age =[15,45,67,32,65,32,65];
+
+for(i = 0; i < age.length; i++){
+    if(age[i] % 2 == 0)
+    console.log(age);
+}
