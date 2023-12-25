e9dde83e1d44b7fbe5b8d1d07945989121e0affb
Prince Soni <132805751+NyXdeity@users.noreply.github.com>
2023-12-25 23:55:59 +0530

    updating

--- index.js
+++ index.js
@@ -219,4 +219,93 @@ console.log("s % b =", s%b);//modulus
 
 /* simple primitives: variables, data types, if/else , loops- for loops
 
+// let first_Name = " Prince ";
+// let age = 18
+// let is_Married = true;
+// console.log("this person's name is" + first_Name +
+// "and their age is "+ age);
+// if(is_Married == true){
+//     console.log(first_Name + "is married.");
+// } else{
+//     console.log(first_Name+"is not married.");
+// }
 
+
+// let answer = 0;// loops:- Q: print the sum of o to nth term?
+// answer += 1+2+3+4+5+6+7+8+9+10;
+// console.log(answer);//ugly way of doing it.
+
+// let n = 0;
+// let answer = 0;
+// for( let i = 0; i<= n; i++ ){
+//     answer = answer + i;
+// }
+// console.log(answer);
+
+//the code below uses the input function and asks for user input:- 
+// const readline = require('readline').createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+//   });
+  
+//   readline.question('Enter a number: ', n => {
+//     n = Number(n); // Convert input to a number
+  
+//     let answer = 0;
+//     for (let i = 0; i <= n; i++) {
+//       answer += i;
+//     }
+  
+//     console.log(answer);
+//     readline.close(); 
+// });
+
+
+
+
+// let first_Name = "Abhishek";// Q1.WAP to greet a person given their first and lasdt name.
+// let last_Name = "soni";
+// console.log("Hello " + first_Name +" "+ last_Name + " how are you?");
+
+
+// let male = true;//Q2.WAP that greets someone based on their gender.
+// if(male == true){
+//     console.log("hello sir");
+// }else{
+//     console.log("hello ma'am");
+// }
+
+
+// let ans = 0;//Q3.WAP to the sum of 0 to 1000.
+// for(i = 0; i <= 1000; i++){
+//     ans = ans + i;
+// }
+// console.log(ans);
+
+*/
+//*complex primitives://arrays and objects:-
+// WAP to print all the even numbers in an array
+
+// const personArray = ["this","is","a","ARRAY"];
+// console.log(personArray[0]);
+
+// const age =[15,45,67,32,65,32,65];
+
+// numberOfpeople = age.length;
+
+// for(i = 0; i < numberOfpeople; i++) {
+    
+//     if(age[i] % 2 == 0){
+//         console.log(age[i]);
+//     }
+// }
+
+// const num = [6,23,54,100,67,68]; //WAP to print the biggest number in an array
+// let biggestNum = [0];//assumes the first element is the largest
+// for(let i = 1 ; i < num.length; i++) {
+    
+//     if(num[i] > biggestNum) {
+//         biggestNum = num[i];//update if a larger number is found 
+//     }
+// }
+// console.log("the biggest number in the array is:- "+biggestNum);
