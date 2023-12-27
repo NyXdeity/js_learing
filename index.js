7e53531953e0bfe61f5e3aa0a846e0ec29985342
Prince Soni <132805751+NyXdeity@users.noreply.github.com>
2023-12-27 23:04:33 +0530

    update

--- index.js
+++ index.js
@@ -331,10 +331,10 @@ console.log("s % b =", s%b);//modulus
 //objects:-
 
 
-const user1 = {
-    firstName: "Prince",
-    gender: "male"
-}// this is the syntax of abojects
-// console.log(user1);//output: { firstName: 'Prince', gender: 'male' }
-console.log(user1["firstName"]);// to get an specific thing in the object  //output: Prince 
-console.log(user1["gender"]);//output: male
+// const user1 = {
+//     firstName: "Prince",
+//     gender: "male"
+// }// this is the syntax of abojects
+// // console.log(user1);//output: { firstName: 'Prince', gender: 'male' }
+// console.log(user1["firstName"]);// to get an specific thing in the object  //output: Prince 
+// console.log(user1["gender"]);//output: male
