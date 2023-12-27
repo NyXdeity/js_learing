52bb02c0f1c31c1db8636019a4dbe84ea61514ef
Prince Soni <132805751+NyXdeity@users.noreply.github.com>
2023-12-27 22:38:52 +0530

    update

--- index.js
+++ index.js
@@ -318,3 +318,11 @@ console.log("s % b =", s%b);//modulus
 //     let gender = genderArray[i];
 //     console.log(person+gender);
 // }
+const personArray = ["prince","abhishek","teena"];//prints out the person based on their arrays:-
+const genderArray = ["male","male","female"];
+ 
+for( i = 0; i < personArray.length; i++){
+    if(genderArray[i] == "male"){
+        console.log(personArray[i]);
+    }
+}
